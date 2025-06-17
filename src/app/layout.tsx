91052b7b17b72cd1d60a7f5af82096bd21df0eae
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2025-06-17 18:15:45 +0300

    Mode toggle button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter, Raleway } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({
   variable: "--font-inter",
